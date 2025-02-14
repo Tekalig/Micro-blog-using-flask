8476b1e7ecdfd48eaa77bf046fe7623270d8f79d
Tekalig <tekahazi06@gmail.com>
2025-02-14 22:34:20 +0300

    update schemas.py to have the comment schemas

--- schemas.py
+++ schemas.py
@@ -7,6 +7,17 @@ class PlainPostSchema(Schema):
     posted_date = fields.Date(dump_only=True)
     updated_date = fields.Date(dump_only=True)
 
+class PlainCommentSchema(Schema):
+    id = fields.Int(dump_only=True)
+    comment = fields.Str(required=True)
+    post_id = fields.Int(required=True)
+    commented_date = fields.Date(dump_only=True)
+    updated_date = fields.Date(dump_only=True)
+
+class UpdateCommentSchema(Schema):
+    comment = fields.Str()
+    updated_date = fields.Date()
+
 class UpdatePostSchema(Schema):
     title = fields.Str()
     content = fields.Str()
